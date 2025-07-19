ed93850624e32d1b5bf808f869d103e70eecb0d2
SREESANKAR-P <sreesankarputhumana@gmail.com>
2025-07-19 17:54:47 +0530

    Added slice

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+    _id: string;
+    name: string;
+    email: string
+}
+
+interface AuthState {
+    user: User | null;
+    isAuthenticated: boolean;
+    loading: boolean;
+    error: string | null
+}
+
+const initialState: AuthState = {
+    user: null,
+    isAuthenticated: false,
+    loading: false,
+    error: null
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        loginStart(state) {
+            state.loading = true;
+            state.error = null;
+        },
+        loginSuccess(state, action: PayloadAction<User>) {
+            state.user = action.payload;
+            state.isAuthenticated = true;
+            state.loading = false;
+        },
+        loginFailure(state, action: PayloadAction<string>) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        logout(state) {
+            state.user = null;
+            state.isAuthenticated = false
+        }
+    }
+})
+
+export const { loginStart, loginSuccess, loginFailure, logout } = authSlice.actions;
+export default authSlice.reducer;
