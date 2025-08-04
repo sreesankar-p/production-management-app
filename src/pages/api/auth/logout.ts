005449ca989dc7e5afdc5d61e8c31a25e6589e02
SREESANKAR-P <sreesankarputhumana@gmail.com>
2025-08-05 00:06:39 +0530

    created logout api

---
+++ src/pages/api/auth/logout.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { clearAuthCookie } from '@/lib/auth';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  clearAuthCookie(res);
+
+  return res.status(200).json({ message: 'Logged out successfully' });
+}
