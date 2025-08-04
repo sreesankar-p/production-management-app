b222e7325c4b8e846a19a9f30161a0e39889da9b
SREESANKAR-P <sreesankarputhumana@gmail.com>
2025-08-05 00:09:18 +0530

    created product index api

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '@/lib/dbConnect';
+import Company from '@/models/Company';
+import { getTokenFromRequest, verifyToken } from '@/lib/auth';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    await dbConnect();
+    const token = getTokenFromRequest(req);
+
+    if (!token) {
+        return res.status(401).json({ message: 'Not authenticated' });
+    }
+
+    try {
+        await verifyToken(token);
+    } catch (error) {
+        console.error(error)
+        return res.status(401).json({ message: 'Not authenticated' });
+    }
+
+    try {
+        switch (req.method) {
+            case 'GET':
+                const companies = await Company.find().populate('createdBy', 'name email');
+                return res.status(200).json(companies);
+
+            case 'POST':
+                const { id } = verifyToken(token) as { id: string };
+                const company = await Company.create({
+                    name: req.body.name,
+                    createdBy: id
+                });
+                return res.status(201).json(company);
+
+            default:
+                return res.status(405).json({ message: 'Method not allowed' });
+        }
+    } catch (error) {
+        console.error(error)
+
+        return res.status(500).json({ message: 'Server error' });
+    }
+}
